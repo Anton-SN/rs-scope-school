0179bec70063c855dc606ca54d4a5df50c294c31
Anton Sivagin <ansn.onward@gmail.com>
2020-10-25 12:50:59 +0200

    feat: add weather

--- momentum/momentum.js
+++ momentum/momentum.js
@@ -2,9 +2,15 @@ const time = document.querySelector(".time");
 const date = document.querySelector(".date");
 const name = document.querySelector(".name");
 const focus = document.querySelector(".focus")
+
 const blockquote = document.querySelector('blockquote');
 const figcaption = document.querySelector('figcaption');
 const btn = document.querySelector('.btn');
+
+const weatherIcon = document.querySelector('.weather-icon');
+const temperature = document.querySelector('.temperature');
+const weatherDescription = document.querySelector('.weather-description');
+const city = document.querySelector('.city');
 
 async function getQuote() {
     const url = `https://cors-anywhere.herokuapp.com/https://api.forismatic.com/api/1.0/?method=getQuote&format=json&lang=en`;
@@ -71,6 +77,41 @@ function focusFocus(e) {
     e.target.innerText = '';
 }
 
+async function getWeather() {
+    if (localStorage.getItem('city') === null) {
+        city.innerHTML = 'Saint Petersburg';
+    } else {
+        city.innerHTML = localStorage.getItem('city');
+    }
+    const id = 'fcf39f3e76637b63b2a590c2e64b3cdb';
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city.innerHTML}&lang=ru&appid=${id}&units=metric`;
+    try {
+        const res = await fetch(url);
+        const data = await res.json();
+        weatherIcon.className = 'weather-icon owf';
+        weatherIcon.classList.add(`owf-${data.weather[0].id}`);
+        temperature.textContent = `${data.main.temp}°C, ${data.wind.speed} m/s`;
+        weatherDescription.textContent = data.weather[0].description;
+    } catch (e) {
+        console.log(e)
+        alert('City not found');
+        localStorage.setItem('city', 'Saint Petersburg');
+        getWeather();
+    }
+}
+
+function setCity(e) {
+    const  { keyCode } = e;
+    if (keyCode === 13) {
+        localStorage.setItem('city', e.target.innerText);
+        city.blur();
+    }
+}
+
+function focusCity(e) {
+    e.target.innerText = '';
+}
+
 name.addEventListener('keypress', setName)
 name.addEventListener('click', focusName)
 name.addEventListener('blur', getName)
@@ -82,7 +123,12 @@ focus.addEventListener('blur', getFocus)
 document.addEventListener('DOMContentLoaded', getQuote);
 btn.addEventListener('click', getQuote);
 
+city.addEventListener('keypress', setCity);
+city.addEventListener('click', focusCity)
+city.addEventListener('blur', getWeather)
+document.addEventListener('DOMContentLoaded', getWeather);
 
 showTime()
 getName()
 getFocus()
+getWeather()
